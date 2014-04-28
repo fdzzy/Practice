40b8bcd26ebe820d0fe0dd294a06f241d551bbc8
Joey Zhang <zhangzhiying1987@163.com>
2014-04-28 22:17:48 +0800

    qt

---
+++ qt/spreadsheet/mainwindow.h
@@ -0,0 +1,64 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class QAction;
+class QLabel;
+class FindDialog;
+class Spreadsheet;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow();
+
+protected:
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+    void newFile();
+    void open();
+    bool save();
+    bool saveAs();
+    void find();
+    void goToCell();
+    void sort();
+    void about();
+    void openRecentFile();
+    void updateStatusBar();
+    void spreadsheetModified();
+
+private:
+    void createAction();
+    void createMenu();
+    void createContextMenu();
+    void createToolBars();
+    void createStatusBar();
+    void readStatusBar();
+    void readSettings();
+    void writeSettings();
+    bool okToContinue();
+    bool loadFile(const QString &fileName);
+    bool saveFile(const QString &fileName);
+    void setCurrentFile(const QString &fileName);
+    void updateRecentFileActions();
+    QString strippedName(const QString &fullFileName);
+
+    Spreadsheet *spreadsheet;
+    FindDialog *findDialog;
+    QLabel *locationLabel;
+    QLabel *formulaLabel;
+    QStringList recentFiles;
+    QString curFile;
+
+    enum { MaxRecentFiles = 5 };
+    QAction *recentFileActions[MaxRecentFiles];
+    QAction *separatorAction;
+    QMenu *fileMenu;
+
+};
+
+#endif
